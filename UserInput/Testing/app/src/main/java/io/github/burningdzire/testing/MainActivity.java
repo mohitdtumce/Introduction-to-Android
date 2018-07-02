9bd76725181b02e389b49d4ae766032d3c225795
burningdzire <mohit.sharma@tatahealth.com>
2018-07-02 12:28:54 +0530

    User Input

--- UserInput/Testing/app/src/main/java/io/github/burningdzire/testing/MainActivity.java
+++ UserInput/Testing/app/src/main/java/io/github/burningdzire/testing/MainActivity.java
@@ -1,4 +1,4 @@
-package io.github.burningdzire.courtcounter;
+package io.github.burningdzire.testing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,15 +7,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    int scoreTeamA = 0;
     public void displayScoreTeamA() {
         TextView t = (TextView) findViewById(R.id.score_team_a);
         t.setText(String.valueOf(scoreTeamA));
@@ -34,32 +32,5 @@ public class MainActivity extends AppCompatActivity {
     public void addOnePointsA(View view) {
         scoreTeamA += 1;
         displayScoreTeamA();
-    }
-
-    public void displayScoreTeamB() {
-        TextView t = (TextView) findViewById(R.id.score_team_b);
-        t.setText(String.valueOf(scoreTeamB));
-    }
-
-    public void addThreePointsB(View view) {
-        scoreTeamB += 3;
-        displayScoreTeamB();
-    }
-
-    public void addTwoPointsB(View view) {
-        scoreTeamB += 2;
-        displayScoreTeamB();
-    }
-
-    public void addOnePointsB(View view) {
-        scoreTeamB += 1;
-        displayScoreTeamB();
-    }
-
-    public void resetScore(View view) {
-        scoreTeamA = 0;
-        scoreTeamB = 0;
-        displayScoreTeamA();
-        displayScoreTeamB();
     }
 }
