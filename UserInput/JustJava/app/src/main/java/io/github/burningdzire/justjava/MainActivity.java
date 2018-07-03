9cef4685710ffe57ff42ec781851e073b22b0304
burningdzire <mohit.sharma@tatahealth.com>
2018-07-03 18:19:33 +0530

    UserInput/JustJava

--- UserInput/JustJava/app/src/main/java/io/github/burningdzire/justjava/MainActivity.java
+++ UserInput/JustJava/app/src/main/java/io/github/burningdzire/justjava/MainActivity.java
@@ -2,10 +2,15 @@ package io.github.burningdzire.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
 
     /**
      * This method is called when the order button is clicked.
@@ -17,37 +22,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 0;
-
     private void displayQuantity() {
         TextView t = (TextView) findViewById(R.id.quantity_text_view);
         t.setText(String.valueOf(quantity));
     }
 
     public void decrementCoffee(View view) {
-        if (quantity > 0) {
+        if (quantity > 1) {
             quantity--;
         }
         displayQuantity();
     }
 
     public void incrementCoffee(View view) {
-        quantity++;
+        if (quantity < 100) {
+            quantity++;
+        }
         displayQuantity();
     }
 
     private int calculatePrice() {
-        return quantity * 5;
+        CheckBox cream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        return (quantity * (5 + (cream.isChecked()?1:0) + (chocolate.isChecked()?2:0)));
     }
 
     private void createOrderSummary(int total) {
-        String orderSummary = "Name: Kaptain Kunal\nQuantity: " + quantity + "\nTotal: $" + total + "\nThank you!!";
+        EditText customerName = (EditText) findViewById(R.id.customer_name);
+        CheckBox cream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        String orderSummary = "Name: " + customerName.getText()
+                + "\nAdd Whipped cream?" + cream.isChecked()
+                + "\nAdd Chocolate?" + chocolate.isChecked()
+                + "\nQuantity: " + quantity + "\nTotal: $" + total + "\nThank you!!";
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(orderSummary);
     }
 
     public void submitOrder(View view) {
         int price = calculatePrice();
+        Log.v("MainActivity", "The value of price is " + price);
         createOrderSummary(price);
     }
 }
