5074354a3dee3ce372bb2405e53ebdab7e47a6d9
burningdzire <mohit.sharma@tatahealth.com>
2018-07-04 18:06:12 +0530

    UserInput/Intent

--- UserInput/JustJava/app/src/main/java/io/github/burningdzire/justjava/MainActivity.java
+++ UserInput/JustJava/app/src/main/java/io/github/burningdzire/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package io.github.burningdzire.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -7,6 +9,8 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,11 +48,12 @@ public class MainActivity extends AppCompatActivity {
     private int calculatePrice() {
         CheckBox cream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
-        return (quantity * (5 + (cream.isChecked()?1:0) + (chocolate.isChecked()?2:0)));
+        return (quantity * (5 + (cream.isChecked() ? 1 : 0) + (chocolate.isChecked() ? 2 : 0)));
     }
 
-    private void createOrderSummary(int total) {
+    private String createOrderSummary(int total) {
         EditText customerName = (EditText) findViewById(R.id.customer_name);
+        EditText customerEmail = (EditText) findViewById(R.id.customer_email);
         CheckBox cream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
         String orderSummary = "Name: " + customerName.getText()
@@ -57,11 +62,28 @@ public class MainActivity extends AppCompatActivity {
                 + "\nQuantity: " + quantity + "\nTotal: $" + total + "\nThank you!!";
         TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
         orderSummaryTextView.setText(orderSummary);
+
+        return orderSummary;
+    }
+
+    public void composeEmail(String address, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, address);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+            Toast.makeText(this,"Intent firing",Toast.LENGTH_SHORT).show();
+        }else{
+
+            Toast.makeText(this,"No firing",Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void submitOrder(View view) {
         int price = calculatePrice();
         Log.v("MainActivity", "The value of price is " + price);
-        createOrderSummary(price);
+        String orderSummary = createOrderSummary(price);
+        composeEmail("mohit.sharmamit@gmail.com", "Hello there");
     }
 }
