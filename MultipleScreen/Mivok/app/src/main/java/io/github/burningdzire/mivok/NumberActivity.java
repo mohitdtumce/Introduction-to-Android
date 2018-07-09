74814e0e1dd4053204ef54ff6719b9a408bebac9
burningdzire <mohit.sharma@tatahealth.com>
2018-07-09 13:10:56 +0530

    ListView|GridView|Spinner + Adapter

--- MultipleScreen/Mivok/app/src/main/java/io/github/burningdzire/mivok/NumberActivity.java
+++ MultipleScreen/Mivok/app/src/main/java/io/github/burningdzire/mivok/NumberActivity.java
@@ -2,6 +2,14 @@ package io.github.burningdzire.mivok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumberActivity extends AppCompatActivity {
 
@@ -9,5 +17,29 @@ public class NumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+//        LinearLayout rootView = (LinearLayout) findViewById(R.id.numberRootView);
+//        for (int i  = 0; i < words.size(); i++)
+//        {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(i));
+//            rootView.addView(wordView);
+//        }
+        int i  = 0;
+        ArrayAdapter<String> itemAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, words);
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(itemAdapter);
     }
 }
