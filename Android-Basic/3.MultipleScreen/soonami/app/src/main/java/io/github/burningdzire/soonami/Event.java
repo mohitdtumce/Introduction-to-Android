8cbac1957434c606b73eec67be5d3c9be56b983c
burningdzire <mohit.sharma@tatahealth.com>
2018-07-12 12:21:28 +0530

    Http Connections | JSON Parsing

---
+++ Android-Basic/3.MultipleScreen/soonami/app/src/main/java/io/github/burningdzire/soonami/Event.java
@@ -0,0 +1,16 @@
+package io.github.burningdzire.soonami;
+
+public class Event {
+
+    public final String title;
+
+    public final long time;
+
+    public final int tsunamiAlert;
+
+    public Event(String eventTitle, long eventTime, int eventTsunamiAlert) {
+        title = eventTitle;
+        time = eventTime;
+        tsunamiAlert = eventTsunamiAlert;
+    }
+}
