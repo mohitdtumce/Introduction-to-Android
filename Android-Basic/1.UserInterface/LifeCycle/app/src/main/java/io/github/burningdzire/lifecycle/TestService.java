de4b8f3a5956d7d7e9c9d84bd25079f4da40874b
burningdzire <mohit.sharma@tatahealth.com>
2018-07-16 16:47:42 +0530

    Service

--- Android-Basic/1.UserInterface/LifeCycle/app/src/main/java/io/github/burningdzire/lifecycle/TestService.java
+++ Android-Basic/1.UserInterface/LifeCycle/app/src/main/java/io/github/burningdzire/lifecycle/TestService.java
@@ -11,14 +11,14 @@ public class TestService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Toast.makeText(this, "Service started", Toast.LENGTH_LONG);
+        Toast.makeText(this, "Service started", Toast.LENGTH_LONG).show();
         return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "Service ended", Toast.LENGTH_LONG);
+        Toast.makeText(this, "Service ended", Toast.LENGTH_LONG).show();
     }
 
     @Nullable
